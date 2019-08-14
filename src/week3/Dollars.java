87759459502877a5092d45c7ff220ca239c774ae
Luke <luke.sexton@my.jcu.edu.au>
2019-08-14 14:16:25 +1000

    Implement Dollars class to convert into currency denominations

---
+++ src/week3/Dollars.java
@@ -0,0 +1,29 @@
+package week3;
+
+import java.util.Scanner;
+
+public class Dollars {
+    public static void main(String[] args) {
+        int amount;
+        int newAmount;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter amount of dollars and cents: ");
+        amount = input.nextInt();
+
+        int twentiesCalculation = amount / 20;
+        System.out.print("Number of 20s: " + twentiesCalculation + "\n");
+        newAmount = amount - (twentiesCalculation * 20);
+
+        int tensCalculation = newAmount / 10;
+        System.out.print("Number of 10s: " + tensCalculation + "\n");
+        newAmount -= (tensCalculation * 10);
+
+        int fivesCalculation = newAmount / 5;
+        System.out.print("Number of 05s: " + fivesCalculation + "\n");
+        newAmount -= (fivesCalculation * 5);
+
+        System.out.print("Number of 01s: " + newAmount);
+
+    }
+}
