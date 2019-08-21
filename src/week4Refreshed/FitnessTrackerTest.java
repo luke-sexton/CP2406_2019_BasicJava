fc6ba35d23c8100c2a4f6da6e7ca50009e3e28c1
Luke <luke.sexton@my.jcu.edu.au>
2019-08-21 16:07:28 +1000

    Implement tests for initial-value constructor

--- src/week4Refreshed/FitnessTrackerTest.java
+++ src/week4Refreshed/FitnessTrackerTest.java
@@ -21,6 +21,25 @@ class FitnessTrackerTest {
         double runningTime = fitnessTracker.getRunningTime();
         System.out.println(runningTime);
         assertEquals(runningTime, 0.00);
+    }
+
+    @Test
+    void testInitialValueConstructor() {
+        FitnessTracker fitnessTracker = new FitnessTracker(true, "January 2 2019", 10.00);
+
+        Boolean status = fitnessTracker.getIsRunning();
+        System.out.println(status);
+        assertEquals(status, true);
+
+        String date = fitnessTracker.getDate();
+        System.out.println(date);
+        assertEquals(date, "January 2 2019");
+
+        double runningTime = fitnessTracker.getRunningTime();
+        System.out.println(runningTime);
+        assertEquals(runningTime, 10.00);
+
+
 
     }
 
