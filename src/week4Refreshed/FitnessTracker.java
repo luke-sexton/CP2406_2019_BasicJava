fa0ecad937e841d97d619c4032cea599b3fb1a22
Luke <luke.sexton@my.jcu.edu.au>
2019-08-21 15:07:44 +1000

    Implement Fitness Tracker class with default values

---
+++ src/week4Refreshed/FitnessTracker.java
@@ -0,0 +1,12 @@
+package week4Refreshed;
+
+public class FitnessTracker {
+
+    private Boolean isRunning = true;
+    private String date = "January 1 2019";
+    private double runningTime = 0.00;
+
+    public Boolean getIsRunning() { return this.isRunning; }
+    public String getDate() { return this.date; }
+    public double getRunningTime() { return this.runningTime; }
+}
