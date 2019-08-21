7a39ca2026fa05592a2372a9618c1f605116a70b
Luke <luke.sexton@my.jcu.edu.au>
2019-08-21 11:40:40 +1000

    Add tests for default constructor

--- src/week4Refreshed/StudentTest.java
+++ src/week4Refreshed/StudentTest.java
@@ -11,6 +11,7 @@ class StudentTest {
         double creditHours = student.getCreditHours();
         double pointsEarned = student.getPointsEarned();
 
+        System.out.println("-- Student --");
         System.out.println(id);
         System.out.println(creditHours);
         System.out.println(pointsEarned);
@@ -21,6 +22,21 @@ class StudentTest {
         Student student = new Student(2, 5, 10);
 
         double gpa = student.calculateGpa();
-        System.out.println("GPA: " + gpa);
+        System.out.println("-- Calculate GPA --");
+        System.out.println(gpa);
+    }
+
+    @Test
+    void initialiser() {
+        Student student = new Student();
+
+        int id = student.getId();
+        double creditHours = student.getCreditHours();
+        double pointsEarned = student.getPointsEarned();
+        System.out.println("-- Initialiser --");
+        System.out.println(id);
+        System.out.println(creditHours);
+        System.out.println(pointsEarned);
+
     }
 }
