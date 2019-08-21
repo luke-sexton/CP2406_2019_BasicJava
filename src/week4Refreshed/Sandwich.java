5f7274bca38ba506e667b2e423ffc4310a71ba4b
Luke <luke.sexton@my.jcu.edu.au>
2019-08-21 10:39:31 +1000

    Fix rearranged code and correct constructor

--- src/week4Refreshed/Sandwich.java
+++ src/week4Refreshed/Sandwich.java
@@ -5,33 +5,32 @@ public class Sandwich {
     private String breadType;
     private double price;
 
-    public Sandwich(String value) {
-        setMainIngredient(value);
-    }
-
-    // Getter Methods
-    public String getMainIngredient() {
-        return mainIngredient;
+    public Sandwich(String mainIngredient, String breadType, double price) {
+        setMainIngredient(mainIngredient);
+        setBreadType(breadType);
+        setPrice(price);
     }
 
     // Setter Methods
     public void setMainIngredient(String value) {
         mainIngredient = value;
     }
-
-    public String getBreadType() {
-        return breadType;
+    public void setPrice(double value) {
+        price = value;
     }
-
     public void setBreadType(String value) {
         breadType = value;
     }
 
+
+    // Getter Methods
+    public String getMainIngredient() {
+        return mainIngredient;
+    }
     public double getPrice() {
         return price;
     }
-
-    public void setPrice(double value) {
-        price = value;
+    public String getBreadType() {
+        return breadType;
     }
 }
