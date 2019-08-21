000c4097de21715e59ce1d931433cd176afd5f65
Luke <luke.sexton@my.jcu.edu.au>
2019-08-21 16:06:56 +1000

    Fix default constructor and implement initial-value constructor

--- src/week4Refreshed/FitnessTracker.java
+++ src/week4Refreshed/FitnessTracker.java
@@ -2,11 +2,31 @@ package week4Refreshed;
 
 public class FitnessTracker {
 
-    private Boolean isRunning = true;
-    private String date = "January 1 2019";
-    private double runningTime = 0.00;
+    private Boolean Running;
+    private String date;
+    private double runningTime;
 
-    public Boolean getIsRunning() { return this.isRunning; }
-    public String getDate() { return this.date; }
-    public double getRunningTime() { return this.runningTime; }
+    // Default Constructor
+    public FitnessTracker() {
+        setRunning(true);
+        setDate("January 1 2019");
+        setRunningTime(0.00);
+    }
+
+    // Initial-Value Constructor
+    public FitnessTracker(Boolean isRunning, String date, double runningTime) {
+        setRunning(isRunning);
+        setDate(date);
+        setRunningTime(runningTime);
+    }
+
+    // Setter Methods
+    public void setRunning(Boolean running) { Running = running; }
+    public void setDate(String date) { this.date = date; }
+    public void setRunningTime(double runningTime) { this.runningTime = runningTime; }
+
+    // Getter Methods
+    public double getRunningTime() { return runningTime; }
+    public String getDate() { return date; }
+    public Boolean getIsRunning() { return Running;}
 }
