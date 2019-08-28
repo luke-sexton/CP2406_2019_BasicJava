c6f6c60b84d4e978884c947bf9610dcd929073c3
Luke <luke.sexton@my.jcu.edu.au>
2019-08-28 21:10:54 +1000

    Implement arrays to utilise sort and reverse methods

--- src/week5/AscendingAndDescending.java
+++ src/week5/AscendingAndDescending.java
@@ -1,75 +1,31 @@
 package week5;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class AscendingAndDescending {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String inputMessage = "Please enter three numbers: ";
-        System.out.println(inputMessage);
+        int userNumOne, userNumTwo, userNumThree;
+        String userInputString = "Please enter three numbers: ";
 
-        int numOne = input.nextInt();
-        int numTwo = input.nextInt();
-        int numThree = input.nextInt();
+        // Get user input
+        System.out.println(userInputString);
 
-        displayOrder(numOne, numTwo, numThree);
+        userNumOne = input.nextInt();
+        userNumTwo = input.nextInt();
+        userNumThree = input.nextInt();
+
+        // Store user input in an array
+        Integer[] numArray = {userNumOne, userNumTwo, userNumThree};
+        Arrays.sort(numArray);
+        System.out.println("Ascending Order: " + Arrays.toString(numArray));
+
+        // Reverse order of list array
+        Collections.reverse(Arrays.asList(numArray)); //
+        System.out.println("Descending Order: " + Arrays.toString(numArray));
+
     }
-
-    public static void displayOrder(int numOne, int numTwo, int numThree) {
-        int lowestNum = getLowestNum(numOne, numTwo, numThree);
-        int highestNum = getHighestNum(numOne, numTwo, numThree);
-        int midNum = getMidNum(numOne, numTwo, numThree, lowestNum, highestNum);
-
-        String ascendingOrder = "Ascending order: " + lowestNum + " " + midNum + " " + highestNum;
-        String descendingOrder = "Descending order: " + highestNum + " " + midNum + " " + lowestNum;
-
-        System.out.println(ascendingOrder);
-        System.out.println(descendingOrder);
-    }
-
-    public static int getLowestNum(int numOne, int numTwo, int numThree) {
-        int lowestNum;
-
-        if (numOne < numTwo && numOne < numThree) {
-            lowestNum = numOne;
-        }
-        else if (numTwo < numOne && numTwo < numThree) {
-            lowestNum = numTwo;
-        }
-        else {
-            lowestNum = numThree;
-        }
-        return lowestNum;
-    }
-
-    public static int getMidNum(int numOne, int numTwo, int numThree, int lowestNum, int highestNum) {
-        int middleNum;
-
-        if ((numTwo == lowestNum && numThree == highestNum) || (numTwo == highestNum && numThree == lowestNum)) {
-            middleNum = numOne;
-        }
-        else if ((numOne == lowestNum && numThree == highestNum) || (numOne == highestNum && numThree == lowestNum)) {
-            middleNum = numTwo ;
-        }
-        else {
-            middleNum = numThree;
-        }
-        return middleNum;
-    }
-
-    public static int getHighestNum(int numOne, int numTwo, int numThree) {
-        int highestNum;
-
-        if (numOne > numTwo && numOne > numThree) {
-            highestNum = numOne;
-        } else if (numTwo > numOne && numTwo > numThree) {
-            highestNum = numTwo;
-        } else {
-            highestNum = numThree;
-        }
-        return highestNum;
-    }
-
-
 }
