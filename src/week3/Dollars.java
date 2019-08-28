da567a70684e0baa6f9833b80b7e8535a5e80d9b
Luke <luke.sexton@my.jcu.edu.au>
2019-08-28 21:47:44 +1000

    Remove repeating statements by implementing an array with for loop

--- src/week3/Dollars.java
+++ src/week3/Dollars.java
@@ -5,27 +5,17 @@ import java.util.Scanner;
 public class Dollars {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        int amount;
-        int remainder;
-
         System.out.print("Please enter the amount: ");
-        amount = input.nextInt();
 
-        remainder = amount % 20;
-        int twenties = (amount - remainder) / 20;
-        amount = remainder;
+        int amount = input.nextInt();
+        int[] notesArray = {20, 10, 5, 1};
 
-        remainder = amount % 10;
-        int tens = (amount - remainder) / 10;
-        amount = remainder;
+        for (int value : notesArray) {
+            int remainder = amount % value;
+            int notes = (amount - remainder) / value;
+            amount = remainder;
 
-        remainder = amount % 5;
-        int fives = (amount - remainder) / 5;
-        amount = remainder;
-
-        int ones = amount;
-
-        System.out.print("Number of 20s: " + twenties + "\nNumber of 10s: " + tens + "\nNumber of 05s: " + fives + "\nNumber of 01s: " + ones + "\n");
+            System.out.println("Number of " + value + "s: " + notes);
+        }
     }
 }
