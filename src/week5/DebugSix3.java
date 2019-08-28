5fa1b8cd98b0b39fc46e5a77774d2620cdd22491
Luke <luke.sexton@my.jcu.edu.au>
2019-08-28 13:46:52 +1000

    Implement correct formatting and syntax for DebugSix3 class

---
+++ src/week5/DebugSix3.java
@@ -0,0 +1,44 @@
+package week5;// DebugSix3.java
+// Prompt user for value to start
+// Value must be between 1 and 20 inclusive
+// At command line, count down to blastoff
+// With a brief pause between each displayed value
+
+import javax.swing.*;
+
+public class DebugSix3 {
+    public static void main(String[] args) {
+        final int MIN = 1;
+        final int MAX = 20;
+        String userNumString;
+        int userNum, val;
+
+        userNumString = JOptionPane.showInputDialog(null,
+                "Enter a number between" + MIN + "and" + MAX + "inclusive");
+        userNum = Integer.parseInt(userNumString);
+
+        while (userNum < MIN || userNum > MAX) {
+            userNumString = JOptionPane.showInputDialog(null,
+                    "Number out of range" +
+                            "\nEnter a number between " + MIN + "and" + MAX + "inclusive");
+            userNum = Integer.parseInt(userNumString);
+        }
+
+        for (val = userNum; val > 0; --val) {
+
+            for (int x = 0; x < 100000; ++x) {
+                for (int y = 0; y < 100000; ++y) {
+                }
+            }
+            System.out.print(val + "  ");
+            // Adjust these numbers for faster or slower performance
+        }
+        System.out.println("Blastoff!");
+    }
+}
+
+
+
+
+
+
