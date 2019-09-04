7bde4474563bee38d4fda64a8c020b83cceaf673
Luke <luke.sexton@my.jcu.edu.au>
2019-09-04 21:39:36 +1000

    Implement array list and display words in reverse order

--- src/week6/DebugEight4.java
+++ src/week6/DebugEight4.java
@@ -2,35 +2,31 @@ package week6;// Allows user to enter a series of words
 // and displays them in reverse order
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 
 public class DebugEight4 {
     public static void main(String[] args) {
-       final String STOP = "XXX";
-       int x = 0, y;
-       String[] array = new String[100];
-       String entry;
-       StringBuffer message = new
-               StringBuffer("The words in reverse order are\n");
+        final String STOP = "XXX";
+        int y;
+        ArrayList<String> array = new ArrayList<String>();
+        String entry;
+        StringBuffer message = new
+                StringBuffer("The words in reverse order are\n");
 
-       entry = JOptionPane.showInputDialog(null,
-               "Enter any word\n" +
-                       "Enter " + STOP + " when you want to stop");
-       while (!entry.equals(STOP)) {
-          array[x] = entry;
-          entry = JOptionPane.showInputDialog(null,
-                  "Enter another word\n" +
-                          "Enter " + STOP + " when you want to stop");
-          x += 1;
-       }
-       for (y = 0; y < x; ++y) {
-
-          message.append(array[y]);
-          message.append("\n");
-
-       }
-
-       JOptionPane.showMessageDialog(null, message);
+        entry = JOptionPane.showInputDialog(null,
+                "Enter any word\n" +
+                        "Entser " + STOP + " when you want to stop");
+        while (!entry.equals(STOP)) {
+            array.add(entry);
+            entry = JOptionPane.showInputDialog(null,
+                    "Enter another word\n" +
+                            "Enter " + STOP + " when you want to stop");
+        }
+        for (y = array.size() - 1; y >= 0; --y) {
+            message.append(array.get(y)).append("\n");
+        }
+        JOptionPane.showMessageDialog(null, message);
     }
 }
 
