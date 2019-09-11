f404afa9639c2eabb149f7af73c858bceddc242c
Luke <luke.sexton@my.jcu.edu.au>
2019-09-11 10:59:40 +1000

    Implement boilerplate code

---
+++ src/week7/Horse.java
@@ -0,0 +1,48 @@
+package week7;
+
+public class Horse {
+    String name;
+    String colour;
+    String birth;
+    int year;
+
+    public Horse(String name, String colour, String birth, int year) {
+        setName(name);
+        setColour(colour);
+        setBirth(birth);
+        setYear(year);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
