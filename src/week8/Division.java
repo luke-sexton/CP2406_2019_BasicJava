2c3881831b42601ed89d7c8ac3d6dcaa548c930f
Luke <luke.sexton@my.jcu.edu.au>
2019-09-18 19:30:28 +1000

    Implement abstract division class

---
+++ src/week8/Division.java
@@ -0,0 +1,12 @@
+package week8;
+
+public abstract class Division {
+    protected String name;
+    protected int accountNumber;
+
+    public Division(String name, int accountNumber) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+    }
+    abstract void display();
+}
