7e098f67ef03b19e9bfd23c15d8be8e51b1a1315
Luke <luke.sexton@my.jcu.edu.au>
2019-09-18 10:49:31 +1000

    Fix debugEleven2 and OceanLiner class

---
+++ src/week8/DebugOceanLiner.java
@@ -0,0 +1,19 @@
+package week8;
+
+public class DebugOceanLiner extends DebugBoat {
+    public DebugOceanLiner() {
+        super("Ocean Liner");
+        setPassengers();
+        setPower();
+    }
+
+    @Override
+    public void setPassengers() {
+        super.passengers = 2400;
+    }
+
+    @Override
+    public void setPower() {
+        super.power = "four engines";
+    }
+}
