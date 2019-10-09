13881a125d21f0bc95a823dea9ef1ac3433464b4
Luke <luke.sexton@my.jcu.edu.au>
2019-10-09 13:19:59 +1000

    Center nested rectangles

--- src/week12/JNestedBoxesPanel.java
+++ src/week12/JNestedBoxesPanel.java
@@ -16,7 +16,7 @@ public class JNestedBoxesPanel extends JPanel {
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         graphics.setColor(Color.BLACK);
-        for (corner = 360, line = 0; line < 360; line += 40, corner -= 20) {
+        for (corner = 350, line = 0; line < 360; line += 40, corner -= 20) {
             graphics.drawRect(corner, corner, line, line);
         }
     }
@@ -25,7 +25,7 @@ public class JNestedBoxesPanel extends JPanel {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.add(new JNestedBoxesPanel());
-        frame.setSize(320, 320);
+        frame.setSize(800, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
